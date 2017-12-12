3494bd28f662468311b81a5562b88cfb70950abf
Jamie Williams <jwilliams492@gmail.com>
2017-12-12 12:09:06 -0800

    completed errorCallBack

--- scripts/errorView.js
+++ scripts/errorView.js
@@ -2,13 +2,19 @@
 
 let errorView = {
     err: function initErrorPage(err) {
-        let template = Handlebars.compile['error-template'];
+        let template = $('.book-view').html();
+        let template = Handlebars.compile('error-template');
 
-        $('.container').hide(); //hides elements with class of container
-        $('.error-view').show(); //shows elements with class of error-view
-        $('#error-message').empty(); //empties content for element with an id of error message
-        $('#error-message').append(book.toHtml()); //
-        
+        $('.container').hide(); //Hides elements with class of container.
+        $('.error-view').show(); //Shows elements with class of error-view.
+        $('#error-message').empty(); //Empties content for element with an id of error message.
+        $('#error-message').append(book.toHtml()); //Renders the err argument into the template, and appends it to an element with an id of error-message.
+
     }
 };
 
+function errorCallBack(err) { //Define a function called errorCallback which takes an error object as an argument when invoked.
+    console.log('Error: ', err); //error logged.
+}
+
+errorView.initErrorPage(errorCallBack); //error passed/
