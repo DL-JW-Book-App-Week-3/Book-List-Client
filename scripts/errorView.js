1b9b3e182b9fa6ab816d6df60ee1d720f57eaadc
Jamie Williams <jwilliams492@gmail.com>
2017-12-12 09:54:01 -0800

    hide elements with class of container and show elements with class of error-view complete

---
+++ scripts/errorView.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let errorView = {
+    err: function initErrorPage(err) {
+        $('.float_form').hide('container');
+        $('.float_form').show('error-view');
+        
+    }
+};
+
