6ace4552a042de81777bfcb4c2a4c85aa99fe769
Jamie Williams <jwilliams492@gmail.com>
2017-12-12 11:07:37 -0800

    correct handlebars.compile

--- scripts/errorView.js
+++ scripts/errorView.js
@@ -2,8 +2,8 @@
 
 let errorView = {
     err: function initErrorPage(err) {
-        let source   = document.getElementById('error-template').innerHTML;
-        let template = Handlebars.templates['error-template'];
+        let template = Handlebars.compile['error-template'];
+
         $('.container').hide(); //hides elements with class of container
         $('.error-view').show(); //shows elements with class of error-view
         $('.error-message').empty(); //empties content for element with an id of error message
