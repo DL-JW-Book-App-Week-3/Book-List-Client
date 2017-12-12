82581edb433a98dd16a9f8ab198a501f5665d14b
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-12-12 11:46:48 -0800

    built constructor and database retrieval for object instantiation

---
+++ scripts/book.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let app = app || {};
+
+(function (module) {
+  function Book(rdo) { //Object Constructor for Books using rdo(rawDataObject)
+    Object.keys(rdo).forEach(key => this[key] = rdo[key]);
+  };
+
+  Book.all = [];
+
+  Book.prototype.toHtml = function () {
+    let template = Handlebars.compile($('#book-template').text());
+
+    return template(this);
+  };
+
+  Book.loadAll = rd => {
+    rd.sort((a,b) => a.title > b.title? 1 : b.title > a.title? -1 : 0) // rd(rawData) sorted by title.
+
+    Book.all = rd.map(bo => new Book(bo)); //bo(bookObject) instantiated for each rd(rawData) entry.
+  };
+
+  Book.fetchAll = call => {
+    $.get('/books_app')
+      .then(res => {
+        Book.loadAll(res);
+        call();
+      })
+  };
+
+  module.Book = Book;
+
+})(app);
