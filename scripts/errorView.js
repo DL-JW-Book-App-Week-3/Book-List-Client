3bce1b3a6beee1b92c7fdedeb773cf85dd7d629e
Jamie Williams <jwilliams492@gmail.com>
2017-12-12 10:51:27 -0800

    added handlebars cdn

--- scripts/errorView.js
+++ scripts/errorView.js
@@ -2,8 +2,12 @@
 
 let errorView = {
     err: function initErrorPage(err) {
-        $('.float_form').hide('container');
-        $('.float_form').show('error-view');
+        let source   = document.getElementById('error-template').innerHTML;
+        let template = Handlebars.templates['error-template'];
+        $('.container').hide(); //hides elements with class of container
+        $('.error-view').show(); //shows elements with class of error-view
+        $('.error-message').empty(); //empties content for element with an id of error message
+        $('.error-message').append(); //
         
     }
 };
