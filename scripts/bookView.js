96bbd4779ff299506be140b1064d7ff53ce47fec
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-12-13 13:49:12 -0800

    built scripting for details page

--- scripts/bookView.js
+++ scripts/bookView.js
@@ -6,9 +6,27 @@ var app = app || {};
 
   let bookView = {};
 
+  // bookView.bookDetails = () => {
+  //   $('.book').on('click', function(e) {
+  //     console.log('clicked', $(this).data('id'))
+  //     // app.Book.fetchOne($(this).data('id'))
+  //     page(`/:${(this).data('id')}`)
+  //     e.preventDefault()
+  //   })
+  // }
+
   bookView.initIndexView = () => {
+    $('#book-details').empty();
     app.Book.all.forEach(b => $('#book-view').append(b.toHtml()));
   };
+
+  bookView.initDetailsView = ctx => {
+    $('#book-view').empty();
+    console.log('initialize object:', ctx)
+    let template = Handlebars.compile($('#details-template').text());
+    $('#book-details').show();
+    $('#book-details').append(template(new app.Book(ctx)))
+  }
 
   module.bookView = bookView;
 
