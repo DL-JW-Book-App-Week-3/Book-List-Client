a9baf4effd6b6fcce620aff1696aee2759c97655
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-12-14 11:00:25 -0800

    built front end functionality for post, put, and delete AJAX requests to server

--- scripts/bookView.js
+++ scripts/bookView.js
@@ -6,26 +6,73 @@ var app = app || {};
 
   let bookView = {};
 
-  // bookView.bookDetails = () => {
-  //   $('.book').on('click', function(e) {
-  //     console.log('clicked', $(this).data('id'))
-  //     // app.Book.fetchOne($(this).data('id'))
-  //     page(`/:${(this).data('id')}`)
-  //     e.preventDefault()
-  //   })
-  // }
+  /////////////////// ** Page Initialization for Page JS ** /////////////////////
 
   bookView.initIndexView = () => {
+    $('.update-view').hide();
+    $('.create-view').hide();
     $('#book-details').empty();
     app.Book.all.forEach(b => $('#book-view').append(b.toHtml()));
   };
 
   bookView.initDetailsView = ctx => {
+    $('.update-view').hide();
+    $('.create-view').hide();
     $('#book-view').empty();
-    console.log('initialize object:', ctx)
+    console.log('initialize object:', ctx);
     let template = Handlebars.compile($('#details-template').text());
     $('#book-details').show();
-    $('#book-details').append(template(new app.Book(ctx)))
+    $('#book-details').append(template(new app.Book(ctx)));
+    $('button[data-id="update-btn"]').on('click', page('/update'));
+    $('button[data-id="delete-btn"]').on('click', page('/delete'));
+  }
+
+  bookView.initCreatePage = () => {
+    $('.update-view').hide();
+    $('#book-view').empty();
+    $('#book-details').empty();
+    $('.create-view').show();
+    $('.create-view').on('submit', bookView.submit);
+  }
+
+  bookView.initUpdatePage = () => {
+    $('.update-view').hide();
+    $('#book-view').empty();
+    $('.update-view').show();
+    $('.create-view').on('submit', bookView.update);
+  }
+
+  /////////////////// ** Book Building for AJAX Requests ** /////////////////////
+
+  bookView.submit = e => {
+    e.preventDefault();
+    let bookfile = new app.Book({
+      title: $('#title').val(),
+      author: $('#author').val(),
+      image_url: $('#url').val(),
+      isbn: $('#isbn').val(),
+      description: $('#description').val()
+    })
+
+    bookfile.insertRecord();
+
+    page('/');
+  }
+
+  bookView.update = e => {
+    e.preventDefault();
+    let updatedBook = new app.Book({
+      book_id: $('.details').data('id'),
+      title: $('#title').val(),
+      author: $('#author').val(),
+      image_url: $('#url').val(),
+      isbn: $('#isbn').val(),
+      description: $('#description').val()
+    })
+
+    updatedBook.updateRecord();
+
+    page('/');
   }
 
   module.bookView = bookView;
